6298596cda8a196750a7a5d9ac0cfe45ddc6a5bc
Adrian Kinberger <adrian.kinberger@gmail.com>
2016-10-12 13:04:02 +0200

    add getAllFor() in SubscriptionEJB

--- src/main/java/search4/ejb/SubscriptionEJB.java
+++ src/main/java/search4/ejb/SubscriptionEJB.java
@@ -4,6 +4,11 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 import search4.daobeans.SubscriptionDAOBean;
+import search4.entities.DisplaySubscriptionEntity;
+import search4.entities.SubscriptionEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Stateless
 public class SubscriptionEJB {
@@ -11,6 +16,19 @@ public class SubscriptionEJB {
 	@EJB
 	private SubscriptionDAOBean subsctiptionDAOBean;
 	
-	
+	public List<DisplaySubscriptionEntity> getAllFor(Integer userId) {
+		List<SubscriptionEntity> list = subsctiptionDAOBean.getAllFor(userId);
+		List<DisplaySubscriptionEntity> displayEntities = new ArrayList<DisplaySubscriptionEntity>();
+		for (SubscriptionEntity se : list) {
+			displayEntities.add(dbEntityToDisplayEntity(se));
+		}
+		return displayEntities;
+	}
+
+	private DisplaySubscriptionEntity dbEntityToDisplayEntity(SubscriptionEntity se) {
+		DisplaySubscriptionEntity displayEntity = new DisplaySubscriptionEntity();
+		displayEntity.setSubscribedMovieId(se.getMovieId());
+		return displayEntity;
+	}
 	
 }
