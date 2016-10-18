bfc5030af6583d35bb38d3f5d008255a3ba2fed1
onordin <nordin.oscar@gmail.com>
2016-10-18 16:59:12 +0200

    delete user function added

--- src/main/java/search4/backingbeans/UserBean.java
+++ src/main/java/search4/backingbeans/UserBean.java
@@ -8,6 +8,7 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import javax.ws.rs.InternalServerErrorException;
 
+import search4.ejb.UserEJB;
 import search4.ejb.interfaces.LocalUser;
 import search4.entities.DisplayUserEntity;
 import search4.entities.UserEntity;
@@ -64,6 +65,16 @@ public class UserBean implements Serializable{
 			message = "Unknown Error";
 			return "full_startpage";
 		}
+	}
+	
+	public String deleteUser() {
+		if(userEJB.deleteUser(displayUserEntity.getId())) {
+			System.out.println("User is deleted.");
+			logOffUser();
+		} else {
+			System.out.println("User is not deleted.");
+		}
+		return "full_startpage";
 	}
 	
 	public String loginUser(){
--- src/main/java/search4/daobeans/UserDAOBean.java
+++ src/main/java/search4/daobeans/UserDAOBean.java
@@ -51,4 +51,24 @@ public class UserDAOBean {
 		}
 		
 	}
+
+	public boolean userExist(int id) {
+		try {
+			UserEntity userEntity = (UserEntity) entityManager.createNamedQuery("UserEntity.getUserById").setParameter("id", id).getSingleResult();
+			return true;
+		} catch (NoResultException nre) {
+			return false;
+		}
+	}
+
+	public boolean deleteUser(int id) {
+		try {
+			entityManager.createNamedQuery("SubscriptionEntity.deleteUser").setParameter("userId", id).executeUpdate();
+			entityManager.createNamedQuery("UserEntity.deleteUserById").setParameter("id", id).executeUpdate();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+		
+	}
 }
